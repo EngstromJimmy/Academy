e739c06ea409af206d8860195749a8b1b242e835
Jimmy Engström <jimmy@apeoholic.se>
2019-02-19 19:51:16 +0100

    Added comments

--- Day2/Express/index.js
+++ Day2/Express/index.js
@@ -1,20 +1,28 @@
-const express=require("express");
-const fs=require("fs");
-const app=express();
-const port=8080;
+const express=require("express"); //Get a reference to the express NPM package
+const fs=require("fs"); //Get a reference to the filesystem
+const app=express(); //Create a web server called "app"
+const port=8080; //Create a variable with portnumber (this can be anything)
 
-app.use(express.static("public"));
+//Tell app that you want it to serve static files from the folder "public"
+//all files in the public folder will be availible through localhost:8080/anyfile.anyextention
+app.use(express.static("public")); 
 
+//If you surf to http://localhost:8080/ will return "Hello"
+// the "/" is the search path just after 8080 in the url above
 app.get("/",(req,res)=>{
     res.send("Hello");
     res.end();
 });
 
+//If you surf to http://localhost:8080/test will return "Hello from test"
+// the "/test" is the search path just after 8080 in the url above
 app.get("/test",(req,res)=>{
     res.send("Hello from test");
     res.end();
 });
 
+//If you surf to http://localhost:8080/image will return the image helloAcademy.png
+// the "/image" is the search path just after 8080 in the url above
 app.get("/image",function (req,res){
     fs.readFile(__dirname + "/helloAcademy.png",
     (err,data)=>{
@@ -23,7 +31,7 @@ app.get("/image",function (req,res){
     })
 });
 
-
+//Start the server
 app.listen(port,()=>
 {console.log("Server is running")});
 
